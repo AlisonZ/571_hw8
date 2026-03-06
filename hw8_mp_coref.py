3fe02bfee8497fd22782d70cd402c16ba29d2183
Alison Zerbe <alisonz@indeed.com>
2026-03-05 17:00:20 -0800

    Cleans up vector features

--- hw8_mp_coref.py
+++ hw8_mp_coref.py
@@ -3,6 +3,9 @@ from utils import load_mention_pairs
 from FeatureVectorModel import FeatureVectorModel
 import json
 import nltk
+import numpy as np
+from numpy.linalg import norm
+
 nltk.download('wordnet_ic')
 from nltk.corpus import wordnet as wn, wordnet_ic
 
@@ -12,14 +15,22 @@ PLURAL_PRONOUNS = ['we', 'our', 'they', 'us']
 
 def get_inputs():
     train_pairs = sys.argv[1]
-    test_pairs = sys.argv[2]
-    vectors_output = sys.argv[3]
-    class_output = sys.argv[4]
-    return train_pairs, test_pairs, vectors_output, class_output
+    embedding_file = sys.argv[2]
+    test_pairs = sys.argv[3]
+    vectors_output = sys.argv[4]
+    class_output = sys.argv[5]
+    return train_pairs, embedding_file, test_pairs, vectors_output, class_output
 
-def read_embedded_vectors():
-    print("Vectors")
-    # /mnt/dropbox/25-26/571W/hw8/dolma_300_2024_1.2M.100_combined.txt
+def read_embedded_vectors(embedding_file):
+    glove_vectors = {}
+    with open(embedding_file, 'r') as f:
+        for line in f:
+            split_line = line.split()
+            word = split_line[0]
+            vector = [float(x) for x in split_line[1:]]
+            glove_vectors[word] = vector
+     
+    return glove_vectors
 
 def get_head(mention):
     tokens = mention['tokens']
@@ -28,44 +39,39 @@ def get_head(mention):
         head_id = token['head_id']
         if head_id not in token_ids:
             return token
-        else:
-            return tokens[-1]
+    return tokens[-1]
 
 def set_distance(fv, antecedent, anaphor):
-    # TODO: is the correc way to calculate this? Do we need to check which comes first?
-    # TODO: if both mentions appear in the sames sentence, value should be 0 --> what does an example like this looks like? How to tell?
-    distance =  antecedent['sentence_num'] - anaphor['sentence_num']
-    if distance > 0:
-        fv.set_distance(distance)
-    else:
-        switched_distance = anaphor['sentence_num'] - antecedent['sentence_num'] 
-        fv.set_distance(switched_distance)
+    # This makes the assumption that the anaphor will always come after the antecedent
+    distance =  anaphor['sentence_num'] - antecedent['sentence_num']
+    fv.set_distance(distance)
 
 def is_pronoun(mention):
     tokens = mention['tokens']
-    if len(tokens) == 1 and (tokens == 'PRP' or tokens == '$PRP'):
+    if len(tokens) == 1 and (tokens[0]['pos'] == 'PRP' or tokens[0]['pos'] == 'PRP$'):
         return 1
     else:
         return 0    
 
-# TODO: only check for DT? anything else? return long strings of all tokens?
 def get_clean_string(mention):
     tokens = mention['tokens']
     clean_str_array = []
+    # TODO: check for DT? or if the word is in the list of determiners/demonstratives? 
+    leading_word_pos = tokens[0]['pos']
+    if leading_word_pos == 'DT':
+        tokens.pop(0)
     for token in tokens:
-        if token['pos'] != 'DT':
-            clean_str_array.append(token['word'])
+        clean_str_array.append(token['word'])
     clean_str = " ".join(clean_str_array)
   
     return clean_str
 
-# TODO: only check for DT and only for "the"??
-# Is this handling this correctly?
 def set_definite_anaphor(anaphor, fv):
     tokens = anaphor['tokens']
     for token in tokens:
         pos = token['pos']
         word = token['word']
+        # TODO: only check for DT and only for "the"??
         if pos == 'DT' and word == 'the':
             fv.set_is_definite_anaphor(1)
             return
@@ -84,13 +90,14 @@ def set_demonstrative_anaphor(anaphor, fv):
     return
 
 def get_number(head):
-    # TODO: check this logic and grammar rules for returning plural and singular
     head_pos = head['pos']
     word = head['word']
 
     if head_pos in PLURAL_POS:
         return 'plural'
-    if (head_pos == 'PRP' or head_pos == '$PRP') and word in PLURAL_PRONOUNS:
+    if (head_pos == 'PRP' or head_pos == 'PRP$') and word in PLURAL_PRONOUNS:
+        if word == 'they':
+            return 'both'
         return 'plural'
     else:
         return 'singular' 
@@ -122,12 +129,48 @@ def get_wordnet_similarity(antecedent_head, anaphor_head):
                 max_sim = sim_score
     return max_sim
 
-# TODO: Implement with the previous assignment
-def get_embedding_similarity():
-    # Float:  Compute the cosine similarity of the two full mention phrases using the static Glove vectors from the previous assignment. 
-    # NOTE: If none of the words in one (or more) the mention phrases are in the embedding file, return 0 for the similarity.
+def get_named_entity(head):
+    pos = head['pos']
+    named_entity = head['named_entity']
+    if pos == 'PRP':
+        return 'person'
+    else:
+        return named_entity
 
-def parse_mps(mps):
+def get_embedding_similarity(glove_vectors, antecedent, anaphor):
+
+    ant_tokens = antecedent['tokens']
+    anaphor_tokens = anaphor['tokens']
+
+    ant_vector = []
+    anaphor_vector = []
+
+  
+
+    ant_average = 0
+    for token in ant_tokens:
+        word = token['word']
+        if word in glove_vectors:
+            word_vector = glove_vectors[word]
+            ant_vector.append(word_vector)
+
+    for token in anaphor_tokens:
+        word = token['word']
+        if word in glove_vectors:
+            word_vector = glove_vectors[word]
+            anaphor_vector.append(word_vector)
+
+    if not ant_vector or not anaphor_vector:
+        return 0
+    
+    ant_average = np.mean(np.array(ant_vector), axis=0)
+    anaphor_average = np.mean(np.array(anaphor_vector), axis=0)
+    
+    cos = np.dot(ant_average, anaphor_average)/(norm(ant_average) * norm(anaphor_average))
+    return cos
+
+def parse_mps(mps, glove_vectors, vectors_output):
+
     for mp in mps:
         fv = FeatureVectorModel()
         antecedent = mp['antecedent']
@@ -169,15 +212,17 @@ def parse_mps(mps):
         # Number agreement
         ant_number = get_number(head=antecedent_head)
         anaphor_number = get_number(head=anaphor_head)
-        if ant_number == anaphor_number:
+        if ant_number == 'both' or anaphor_number == 'both':
             fv.set_number_agreement(1)
-        else: 
-            fv.set_number_agreement(0)
+        else:
+            if ant_number == anaphor_number:
+                fv.set_number_agreement(1)
+            else: 
+                fv.set_number_agreement(0)
 
         # Same named entity
-        # TODO: is this the only thing to check with named entity? all are __
-        antecendent_entity = antecedent_head['named_entity']
-        anaphor_entity = anaphor_head['named_entity']
+        antecendent_entity = get_named_entity(antecedent_head)
+        anaphor_entity = get_named_entity(anaphor_head)
         if antecendent_entity == anaphor_entity:
             fv.set_same_named_entity(1)
         else:
@@ -187,16 +232,17 @@ def parse_mps(mps):
         wordnet_similarity = get_wordnet_similarity(antecedent_head, anaphor_head)
 
         # Embedding similarity 
-        # TODO: Implement and call
-        # get_embedding_similarity()
-        
+        cos = get_embedding_similarity(glove_vectors, antecedent, anaphor)
+        fv.set_embedding_similarity(cos)
 
+        print_feature_vectors(fv, vectors_output)
     
 def main():
-    train_pairs, test_pairs, vectors_output, class_output = get_inputs()
-    # read_embedded_vectors()
+    train_pairs, embedding_file, test_pairs, vectors_output, class_output = get_inputs()
+    glove_vectors = read_embedded_vectors(embedding_file)
+
     train_mps = load_mention_pairs(train_pairs)
-    parse_mps(mps = train_mps)
+    parse_mps(mps = train_mps, glove_vectors=glove_vectors, vectors_output = vectors_output)
 
 if __name__ =='__main__':
     main()
--- hw8_mp_coref.sh
+++ hw8_mp_coref.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# 1: <training_file> ./data/train_pairs.json
-# 2: <testing_file> ./data/test_pairs.json
-# 3: <output_vector_file>: hw8_vectors_output.txt
-#4: <output_classification_filename>: hw8_classification_output.txt
-# ./hw8_mp_coref.sh ./data/train_pairs.json /data/test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt
-python3 hw8_mp_coref.py $1 $2 $3 $4
+# 1: <training_file> ./data/train_pairs.jsonx
+#2: <embedding_file> ./dolma_300_2024_1.2M.100_combined.txt
+# 3: <testing_file> ./data/test_pairs.json
+# 4: <output_vector_file>: hw8_vectors_output.txt
+#5: <output_classification_filename>: hw8_classification_output.txt
+# ./hw8_mp_coref.sh ./data/toy_train_pairs.json /data/toy_test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt
+python3 hw8_mp_coref.py $1 $2 $3 $4 $5
