4ae3a0e9e4339c7a11f38cb27336bd2b45db1656
Alison Zerbe <alisonz@indeed.com>
2026-03-06 09:29:57 -0800

    updates shell script and adds condor file

---
+++ hw8.cmd
@@ -0,0 +1,8 @@
+executable = hw8_mp_coref.sh
+output = hw8_parse.out
+error = hw8_parse.error
+log = hw8_parse.log
+notification = complete
+arguments = "/mnt/dropbox/25-26/571W/hw8/data/train_pairs.json /mnt/dropbox/25-26/571W/hw8/dolma_300_2024_1.2M.100_combined.txt /mnt/dropbox/25-26/571W/hw8/data/test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt"
+request_memory = 2*1024
+queue
--- hw8_mp_coref.sh
+++ hw8_mp_coref.sh
@@ -4,5 +4,6 @@
 # 3: <testing_file> ./data/test_pairs.json
 # 4: <output_vector_file>: hw8_vectors_output.txt
 #5: <output_classification_filename>: hw8_classification_output.txt
-# ./hw8_mp_coref.sh ./data/toy_train_pairs.json /data/toy_test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt
+# ./hw8_mp_coref.sh . /data/toy_test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt
+# ./hw8_mp_coref.sh /mnt/dropbox/25-26/571W/hw8/data/train_pairs.json /mnt/dropbox/25-26/571W/hw8/dolma_300_2024_1.2M.100_combined.txt /mnt/dropbox/25-26/571W/hw8/data/test_pairs.json hw8_vectors_output.txt hw8_classification_output.txt
 python3 hw8_mp_coref.py $1 $2 $3 $4 $5
